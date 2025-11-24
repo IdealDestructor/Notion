85d1601271fe9c9ff53eebcd5b5173cc4f809d4f
Francis <idealdestructor@gmail.com>
2025-11-24 19:57:53 +0800

    up

--- themes/matery/components/Hero.js
+++ themes/matery/components/Hero.js
@@ -58,7 +58,8 @@ const Hero = props => {
       <div className='text-white absolute flex flex-col h-full items-center justify-center w-full '>
         {/* 站点标题 */}
         <div className='text-4xl md:text-5xl shadow-text'>
-          {siteInfo?.title || siteConfig('TITLE')}
+          {/* {siteInfo?.title || siteConfig('TITLE')} */}
+          生活在别处
         </div>
         {/* 站点欢迎语 */}
         <div className='mt-2 h-12 items-center text-center shadow-text text-white text-lg'>
